869bd8beb0da6012f68858910d6771e0bb32847b
debbie <nypdpa@gmail.com>
2020-02-21 23:51:53 +1100

    Second test for user validator and reconfigure travis CI

--- UserManagementTests/UserValidatorTest.cs
+++ UserManagementTests/UserValidatorTest.cs
@@ -6,11 +6,23 @@ namespace UserManagementTests
 {
     public class UserValidatorTest
     {
+        private readonly UserValidator _validator;
+
+        public UserValidatorTest()
+        {
+            _validator = new UserValidator();
+        }
+
         [Fact]
         public void Should_Have_Error_When_Name_Is_Null()
         {
-            var validator = new UserValidator();
-            validator.ShouldHaveValidationErrorFor(user => user.Name, null as string);
+            _validator.ShouldHaveValidationErrorFor(user => user.Name, null as string);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Name_Is_Specified()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(user => user.Name, "Jeremy");
         }
     }
 }
