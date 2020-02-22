351f13abeb440196452c594bae11067191e2b4b9
debbie <nypdpa@gmail.com>
2020-02-22 14:41:36 +1100

    Set up swagger and fix up seed script for testing

--- UserManagement/Startup.cs
+++ UserManagement/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using UserManagement.Models;
 
 namespace UserManagement
@@ -29,7 +30,12 @@ namespace UserManagement
                     connectionString
                 )
             );
-
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "User Management API", Version = "v1" });
+            });
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
@@ -48,6 +54,13 @@ namespace UserManagement
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Management API V1");
+            });
         }
     }
 }
--- UserManagement/dbscripts/seed.sql
+++ UserManagement/dbscripts/seed.sql
@@ -7,12 +7,12 @@ CREATE TABLE users
     name  VARCHAR (50)  NOT NULL,
     email  VARCHAR (100)  NOT NULL,
     salary NUMERIC NOT NULL,
-    expenses NUMERIC NOT NULL,
+    expenses NUMERIC NOT NULL
 );
 
 ALTER TABLE users OWNER TO useradmin;
 
-Insert into users(name,email) values('Title 1','Email1',1000,0);
-Insert into users(name,email) values('Title 2','Email2',2000,1000);
-Insert into users(name,email) values('Title 3','Email3',3000,1000);
-Insert into users(name,email) values('Title 4','Email4',5000,100);
+Insert into users(name,email,salary,expenses) values('Title 1','Email1',1000,0);
+Insert into users(name,email,salary,expenses) values('Title 2','Email2',2000,1000);
+Insert into users(name,email,salary,expenses) values('Title 3','Email3',3000,1000);
+Insert into users(name,email,salary,expenses) values('Title 4','Email4',5000,100);
