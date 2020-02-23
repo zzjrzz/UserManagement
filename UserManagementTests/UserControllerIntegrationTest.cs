1566dac056ea8fb4dfe2b1b4e6482f1e0dd09628
debbie <nypdpa@gmail.com>
2020-02-23 15:57:08 +1100

    Refactor integration tests on user controller

--- UserManagementTests/UserControllerIntegrationTest.cs
+++ UserManagementTests/UserControllerIntegrationTest.cs
@@ -13,28 +13,19 @@ namespace UserManagementTests
             _webApplicationFactory = webApplicationFactory;
         }
 
-        [Fact]
-        public async Task Get_User_Is_Successful()
+        [Theory]
+        [InlineData("/api/user")]
+        [InlineData("/api/user/jeremy@example.com")]
+        public async Task Get_User_Endpoints_Are_Successful(string requestUri)
         {
             var client = _webApplicationFactory.CreateClient();
             
-            var response = await client.GetAsync("/api/user");
+            var response = await client.GetAsync(requestUri);
             
             response.EnsureSuccessStatusCode();
             Assert.Equal("application/json; charset=utf-8", 
                 response.Content.Headers.ContentType.ToString());
         }
 
-        [Fact]
-        public async Task Get_User_By_Email_Is_Successful()
-        {
-            var client = _webApplicationFactory.CreateClient();
-
-            var response = await client.GetAsync("/api/user/jeremy@example.com");
-
-            response.EnsureSuccessStatusCode();
-
-            Assert.Equal("application/json; charset=utf-8", 
-                response.Content.Headers.ContentType.ToString());        }
     }
 }
